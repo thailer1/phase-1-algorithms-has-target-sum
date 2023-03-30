3edeffd871d440c4c5e2e2d4f4814f881c26f6ba
Thailer <thailer.palmer@gmail.com>
2023-03-30 16:01:54 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
-
+    for (let i = 0; i < array.length ; i++){
+    const end = target - array[i];
+    for ( let j = i +1 ; j< array.length ; j++){
+    if ( array[j] === end ) return true
+        }
+      }
+return false;
+  }
 /* 
   Write the Big O time complexity of your function here
+   Big 0 time complexity is 0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  
 */
 
 /*
   Add written explanation of your solution here
+  Iterate through the array for two numbers. Returns two numbers that will equal the target number.
 */
 
 // You can run `node index.js` to view these console logs
